af510ef889132da2dc5429effac9616880e63ce9
Juraj <jborza@gmail.com>
2016-12-07 11:51:43 +0100

    added HelloServlet

---
+++ src/example/nosql/HelloServlet.java
@@ -0,0 +1,43 @@
+package example.nosql;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.Part;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.cloudant.client.api.Database;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.internal.LinkedTreeMap;
+
+@Path("/hello")
+public class HelloServlet {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get() throws Exception {
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Date date = new Date();
+		String formatted = dateFormat.format(date);
+		return Response.ok(formatted).build();
+	}
+}
